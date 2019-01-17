40a673dab817900bc4ead12707c1116bf16248e3
leonardo15043 <lahernandez15043@gmail.com>
2019-01-17 18:06:04 -0500

    Se implemento el operador MAP en el servicio y Se creo el Buscador | search.component.html

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class SpotifyService {
@@ -11,10 +12,26 @@ export class SpotifyService {
   getNewReleases(){
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCScORqi3PPV9ocy3PsT4vijiFK9QfrWuRo_v79mV0m55vBsP4kRUh8aXukaanmqVObsK4wwBnIdhwyvBE'
+      'Authorization': 'Bearer BQC8vMzDHu_TtzOp5tc8pSZ_t_V5n0gUlDUHMuKBcAw7VWnmdDJfg9qV80BQIWYFrJVub3V-PuNW9To-Wyo'
     });
 
-    return this.http.get("https://api.spotify.com/v1/browse/new-releases", { headers });
+    return this.http.get("https://api.spotify.com/v1/browse/new-releases", { headers })
+      .pipe( map( data =>{
+        return data['albums'].items;
+      }))
+
+  }
+
+  getArtista(termino){
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQC8vMzDHu_TtzOp5tc8pSZ_t_V5n0gUlDUHMuKBcAw7VWnmdDJfg9qV80BQIWYFrJVub3V-PuNW9To-Wyo'
+    });
+
+    return this.http.get('https://api.spotify.com/v1/search?type=artist&q='+termino, { headers })
+      .pipe( map( data =>{
+            return data['artists'].items;
+          }))
 
   }
 
